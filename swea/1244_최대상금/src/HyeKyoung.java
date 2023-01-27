1dbe8be70348211ad06b344f691d424e750143e4
khkkhk4095 <122837463+khkkhk4095@users.noreply.github.com>
2023-01-27 09:56:10 +0900

    늦어서미안

---
+++ swea/1244_최대상금/src/HyeKyoung.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class HyeKyoung {
+	static int n;
+	static char[] ch;
+	static int result = 0;
+
+	static void change(int idx, int cnt) {
+		if (cnt == n) {
+			result = Math.max(result, Integer.parseInt(String.valueOf(ch)));
+			return;
+		}
+
+		for (int i = idx; i < ch.length - 1; i++) {
+			for (int j = i + 1; j < ch.length; j++) {
+				swap(i, j);
+				change(i, cnt + 1);
+				swap(i, j);
+			}
+		}
+	}
+
+	static void swap(int i, int j) {
+		char tmp = ch[i];
+		ch[i] = ch[j];
+		ch[j] = tmp;
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			result = 0;
+			String s = sc.next();
+			ch = new char[s.length()];
+			ch = s.toCharArray();
+			n = sc.nextInt();
+			change(0, 0);
+
+			System.out.printf("#%d %d\n", test_case, result);
+		}
+	}
+}
---
+++ swea/2806_N-Queen/src/HyeKyoung.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class HyeKyoung {
+	public static int[] a;
+	public static int cnt;
+	public static int n;
+
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			n = sc.nextInt();
+			cnt = 0;
+
+			for (int i = 0; i < n; i++) {
+				a = new int[n];
+				a[0] = i;
+				nQueen(1);
+			}
+			System.out.printf("#%d %d\n", test_case, cnt);
+		}
+	}
+
+	public static void nQueen(int c) {
+		if (c == n) {
+			cnt++;
+			return;
+		}
+
+		for (int i = 0; i < n; i++) {
+			a[c] = i;
+			if (check(c))
+				nQueen(c + 1);
+		}
+	}
+
+	public static boolean check(int c) {
+		boolean exist = true;
+		for (int i = 0; i < c; i++) {
+			if (a[i] == a[c])
+				exist = false; // 같은 행에 퀸이 존재
+			else if (Math.abs(i - c) == Math.abs(a[i] - a[c]))
+				exist = false; // 대각선 방향에 퀸
+		}
+
+		return exist;
+	}
+}
