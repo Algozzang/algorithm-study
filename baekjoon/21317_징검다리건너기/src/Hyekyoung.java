3e2386b6d25e37a255a85f73a846127d04b037ab
김혜경 <khkkhk4095@gmail.com>
2023-03-19 21:23:06 +0900

    문제풀이: 11726. 2xn 타일링 이게 맞네네?

--- baekjoon/21317_징검다리건너기/src/Hyekyoung.java
+++ baekjoon/21317_징검다리건너기/src/Hyekyoung.java
@@ -19,12 +19,14 @@ public class Hyekyoung {
 		int k = Integer.parseInt(br.readLine());
 		for(int i=1; i<n; i++) dp[i][0] = dp[i][1] = max;
 		dp[1][0] = jump[0][0];
-		dp[2][0] = Math.min(jump[0][1], dp[1][0] + jump[1][0]);
-		
-		for(int i=3; i<n; i++) {
-			dp[i][0] = Math.min(dp[i-2][0] + jump[i-2][1], dp[i-1][0] + jump[i-1][0]);
-			dp[i][1] = Math.min(dp[i-3][0]+k, dp[i][1]); //세칸 건너뛴 친구랑 이미 왕크게 뛴 애랑 둘 중에 큰거
-			dp[i][1] = Math.min(dp[i][1], Math.min(dp[i-2][1] + jump[i-2][1], dp[i-1][1] + jump[i-1][0]));
+		if(n>1) {
+			dp[2][0] = Math.min(jump[0][1], dp[1][0] + jump[1][0]);
+			
+			for(int i=3; i<n; i++) {
+				dp[i][0] = Math.min(dp[i-2][0] + jump[i-2][1], dp[i-1][0] + jump[i-1][0]);
+				dp[i][1] = Math.min(dp[i-3][0]+k, dp[i][1]); //세칸 건너뛴 친구랑 이미 왕크게 뛴 애랑 둘 중에 큰거
+				dp[i][1] = Math.min(dp[i][1], Math.min(dp[i-2][1] + jump[i-2][1], dp[i-1][1] + jump[i-1][0]));
+			}
 		}
 		
 		System.out.println(Math.min(dp[n-1][0], dp[n-1][1]));
