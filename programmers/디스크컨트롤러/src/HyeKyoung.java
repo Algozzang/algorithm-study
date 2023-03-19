3407c4e74d9139e86d4d7aa67ba322c0399b808e
김혜경 <khkkhk4095@gmail.com>
2023-03-19 17:31:05 +0900

    주석추가

--- programmers/디스크컨트롤러/src/HyeKyoung.java
+++ programmers/디스크컨트롤러/src/HyeKyoung.java
@@ -7,22 +7,15 @@ public class HyeKyoung {
 		Solution s1 = new Solution();
 		int s1Result = s1.solution(new int[][] {{0, 3}, {1, 9}, {2, 6}});
 		System.out.println(s1Result);
-		
 		// expected result = 9;
 	}
 }
 
 class Solution {
-	PriorityQueue<int[]> pQueue = new PriorityQueue<>((o1, o2)->o1[1]-o2[1]);
-	int[][] request;
-	
     public int solution(int[][] jobs) {
+    	PriorityQueue<int[]> pQueue = new PriorityQueue<>((o1, o2)->o1[1]-o2[1]);
     	int end=0, idx=0, cnt=0, sum=0;
     	int len = jobs.length;
-    	request = new int[jobs.length][];
-    	for(int i=0; i<jobs.length; i++) {
-    		request[i] = jobs[i].clone();
-    	}
     	
     	Arrays.sort(jobs, new Comparator<int[]>() {
 			@Override
@@ -33,16 +26,19 @@ class Solution {
     	
     	while(cnt<len) {
     		while(idx<len && jobs[idx][0]<=end) {
+    			// 끝나기 전에 요청 들어온 애 pq에 추가
     			pQueue.add(jobs[idx++]);
     		}
     		
     		if(pQueue.isEmpty()) {
+    			//다른애 들어오는 시간으로 점프
     			end = jobs[idx][0];
     			continue;
     		}
     		
+    		//수행시간 짧은 순으로 먼저 처리
     		int[] curJob = pQueue.poll();
-    		sum+= end + curJob[1] - curJob[0];
+    		sum+= end + curJob[1] - curJob[0]; //요청시간~수행끝 시간
     		cnt++;
     		end+=curJob[1];
     	}
