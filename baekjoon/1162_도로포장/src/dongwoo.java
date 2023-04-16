4584534c7f8dd378c857b17d146aff26ed92c699
고동우 <shp05179@gmail.com>
2023-04-16 12:29:07 +0900

    11주차 문제 도로포장 다시풀기

--- baekjoon/1162_도로포장/src/dongwoo.java
+++ baekjoon/1162_도로포장/src/dongwoo.java
@@ -18,7 +18,7 @@ public class dongwoo {
 		for (int i = 2; i < N + 1; i++) {
 			Arrays.fill(distance[i], Long.MAX_VALUE);
 		}
-		List<int[]>[] roads = new ArrayList[N + 1];
+		List<long[]>[] roads = new ArrayList[N + 1];
 		for (int i = 0; i < N + 1; i++) {
 			roads[i] = new ArrayList<>();
 		}
@@ -27,35 +27,35 @@ public class dongwoo {
 			int city1 = Integer.parseInt(st.nextToken());
 			int city2 = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-			roads[city1].add(new int[] { city1, city2, cost, 0 });
-			roads[city2].add(new int[] { city2, city1, cost, 0 });
+			roads[city1].add(new long[] { city1, city2, cost, 0 });
+			roads[city2].add(new long[] { city2, city1, cost, 0 });
 		}
 		// 포장 안하는것, 가까운 거리 우선 다익스트라
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[3] == o2[3] ? o1[2] - o2[2] : o1[3] - o2[3]);
-		pq.add(new int[] { 0, 1, 0, 0 });
+		PriorityQueue<long[]> pq = new PriorityQueue<>((o1, o2) -> Long.compare(o1[2], o2[2]));
+		pq.add(new long[] { 0, 1, 0, 0 });
 		long min = Long.MAX_VALUE;	// 현재까지 목적지 도달 했을때 든 시간중 가장작은값 저장 
+		long[] now;
+		long[] road;
 		while (!pq.isEmpty()) {
-			int[] now = pq.poll();
-			for (int[] road : roads[now[1]]) {
+			now = pq.poll();
+			if (distance[(int) now[1]][(int) now[3]]<now[2]) {
+				continue;
+			}
+			for (int i=0; i<roads[(int) now[1]].size(); i++) {
+				road = roads[(int) now[1]].get(i);
 				// 비포장 
-				if (distance[road[1]][now[3]] > distance[road[0]][now[3]] + road[2]
-						&& min > distance[road[0]][now[3]] + road[2]) {
-					distance[road[1]][now[3]] = distance[road[0]][now[3]] + road[2];
-					pq.add(new int[] { road[0], road[1], road[2], now[3] });
+				if (distance[(int) road[1]][(int) now[3]] > now[2] + road[2]) {
+					distance[(int) road[1]][(int) now[3]] = distance[(int) road[0]][(int) now[3]] + road[2];
+					pq.add(new long[] { road[0], road[1], distance[(int) road[1]][(int) now[3]], now[3] });
 				}
 				// 포장 
-				if (now[3] + 1 <= K && distance[road[1]][now[3] + 1] > distance[road[0]][now[3]]
-						&& min > distance[road[0]][now[3]]) {
-					distance[road[1]][now[3] + 1] = distance[road[0]][now[3]];
-					if (distance[road[1]][now[3] + 1] > distance[road[1]][K + 1]) {
-						continue;
-					}
-					distance[road[1]][K + 1] = Math.min(distance[road[1]][K + 1], distance[road[1]][now[3] + 1]);
-					pq.add(new int[] { road[0], road[1], road[2], now[3] + 1 });
+				if (now[3] + 1 <= K && distance[(int) road[1]][(int) (now[3] + 1)] > now[2]) {
+					distance[(int) road[1]][(int) (now[3] + 1)] = now[2];
+					pq.add(new long[] { road[0], road[1], now[2], now[3] + 1 });
 				}
 				// min 값 업데이트 
 				if (road[1] == N) {
-					min = Math.min(min, Math.min(distance[N][now[3] + 1], distance[N][now[3]]));
+					min = Math.min(min, Math.min(distance[N][(int) (now[3] + 1)], distance[N][(int) now[3]]));
 				}
 			}
 		}
