c3a5c2a5ed909744e05e88c11d5afe494bdebc8f
김혜경 <khkkhk4095@gmail.com>
2023-04-16 23:57:31 +0900

    문제풀이:프로그래머스 베스트앨범

---
+++ programmers/베스트앨범/src/Hyekyoung.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Hyekyoung {
+	public static void main(String[] args) {
+		String[] genres = new String[] {"pop", "pop", "pop", "rap", "rap" };
+		int[] plays = new int[] { 45, 50, 40, 60, 70 };
+		Solution s1 = new Solution();
+		System.out.println(Arrays.toString(s1.solution(genres, plays)));
+		if (Arrays.equals(new int[] {4,1,3,0}, s1.solution(genres, plays))) {
+			System.out.println("통과");
+		} else {
+			System.out.println("땡");
+		}
+	}
+}
+
+class Song implements Comparable<Song>{
+	int play, num;
+
+	public Song(int play, int num) {
+		super();
+		this.play = play;
+		this.num = num;
+	}
+
+	@Override
+	public int compareTo(Song o) {
+		return o.play - this.play;
+	}
+}
+
+class Genre implements Comparable<Genre>{
+	String genre;
+	int playCnt;
+	
+	public Genre(String genre, int playCnt) {
+		super();
+		this.genre = genre;
+		this.playCnt = playCnt;
+	}
+
+	@Override
+	public int compareTo(Genre o) {
+		return o.playCnt - this.playCnt;
+	}
+}
+
+class Solution {
+	public int[] solution(String[] genres, int[] plays) {
+		
+		Map<String, List<Song>> genre = new HashMap<>();
+		Map<String, Integer> playCnt = new HashMap<>();
+		List<Genre> gList = new ArrayList<>();
+		List<Integer> ans = new ArrayList<>();
+		int size = genres.length;
+		
+		for(int i=0; i<size; i++) {
+			if(!playCnt.containsKey(genres[i])) {
+				genre.put(genres[i], new ArrayList<>());
+				playCnt.put(genres[i], plays[i]);
+			}
+			genre.get(genres[i]).add(new Song(plays[i], i));
+			playCnt.put(genres[i], playCnt.get(genres[i])+plays[i]);
+		}
+		for (String s : playCnt.keySet()) {
+			System.out.println(s+" "+playCnt.get(s));
+			gList.add(new Genre(s, playCnt.get(s)));
+		}
+		
+		Collections.sort(gList);
+		for (Genre g : gList) {
+			Collections.sort(genre.get(g.genre));
+			ans.add(genre.get(g.genre).get(0).num);
+			if(genre.get(g.genre).size()>1) {
+				ans.add(genre.get(g.genre).get(1).num);
+			}
+		}
+		
+		int[] answer = new int[ans.size()];
+		for(int i=0; i<ans.size(); i++) answer[i] = ans.get(i);
+		return answer;
+	}
+}
